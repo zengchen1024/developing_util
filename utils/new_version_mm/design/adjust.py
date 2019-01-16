7efd2e58dd97763fae150e950f924871aa5b5822
zengchen1024 <chenzeng2@huawei.com>
2019-01-16 18:00:23 +0800

    refactor design

---
+++ utils/new_version_mm/design/adjust.py
@@ -0,0 +1,135 @@
+import re
+
+
+class _Tree(object):
+    def __init__(self, properties):
+        self._p = properties
+
+        for v in self._p.values():
+            v.parent = self
+
+    def child(self, key):
+        if key in self._p:
+            return self._p[key]
+
+        raise Exception("no child with key(%s)" % key)
+
+    def add_child(self, child):
+        self._p[child.get_item("name")] = child
+
+    def delete_child(self, child):
+        self._p.pop(child.get_item("name"))
+
+    def find_param(self, keys):
+        obj = self
+        for k in keys.split('.'):
+            obj = obj.child(k.strip())
+
+        return obj
+
+    def rename(self, argv):
+        ex_msg = "Execute cmd(rename %s) failed, " % argv
+
+        v = argv.split(" ")
+        if len(v) != 2:
+            raise Exception("%smust input node and its new name" % ex_msg)
+
+        p = self.find_param(v[0])
+        parent = p.parent
+        try:
+            self._raise_if_duplicate_name(parent, v[1])
+        except Exception as ex:
+            raise Exception("%s%s" % (ex_msg, ex))
+
+        # delete old index, because the parent stores the child in map
+        parent.delete_child(p)
+
+        # rename and add node with new index
+        p.set_item("name", v[1])
+        parent.add_child(p)
+
+    def delete(self, node):
+        p = self.find_param(node)
+        p.parent.delete_child(p)
+
+    def move(self, argv):
+        ex_msg = "Execute cmd(move %s) failed, " % argv
+
+        v = argv.split(" ")
+        if len(v) != 2:
+            raise Exception("%smust input node and new location of parent, "
+                            "input root if move to first level" % ex_msg)
+
+        parent = self if v[1] == "root" else self.find_param(v[1])
+
+        try:
+            self._raise_if_duplicate_name(parent, v[0].split(".")[-1])
+        except Exception as ex:
+            raise Exception("%s%s" % (ex_msg, ex))
+
+        parent.add_child(self.find_param(v[0]))
+        self.delete(v[0])
+
+    def merge(self, argv):
+        """ merge node1 with node2 """
+
+        ex_msg = "Execute cmd(merge %s) failed, " % argv
+
+        v = argv.split(" ")
+        if len(v) != 2:
+            raise Exception("%smust input node1 and node2" % ex_msg)
+
+        self.find_param(v[0]).merge(self.find_param(v[1]), _merge_to, None)
+
+        self.delete(v[1])
+
+    def _raise_if_duplicate_name(self, parent, node_name):
+        try:
+            parent.child(node_name)
+        except Exception:
+            return
+
+        n = "root_node" if parent == self else parent.get_item("name")
+
+        raise Exception("the name(%s) is exist in parameter(%s)" % (
+                            node_name, n))
+
+
+def _merge_to(node2, node1, level):
+    if not (node1 and node2):
+        return
+
+    op1 = set(node1.path.keys())
+    op2 = set(node2.path.keys())
+    if op1.intersection(op2):
+        raise Exception("can merge the parameter of same "
+                        "api(%s)" % " ".join(op1.intersection(op2)))
+
+    node1.path.update(node2.path)
+
+    if node2.get_item("required"):
+        node1.set_item("required", True)
+
+
+def adjust(adjust_cmds, properties):
+    rn = _Tree(properties)
+    fm = {
+        'rename': rn.rename,
+        'delete': rn.delete,
+        'merge': rn.merge,
+        'move': rn.move
+    }
+
+    for cmds in adjust_cmds:
+        if isinstance(cmds, str):
+            cmds = [cmds]
+
+        for cmd in cmds:
+            cmd = re.sub(r" +", " ", cmd)
+            i = cmd.find(" ")
+            f = fm.get(cmd[:i])
+            if not f:
+                raise Exception("Execute cmd(%s) failed, "
+                                "unknown cmd(%s)" % (cmd, cmd[:i]))
+
+            f(cmd[(i + 1):])
---
+++ utils/new_version_mm/design/main.py
@@ -0,0 +1,68 @@
+import pystache
+import sys
+sys.path.append("..")
+
+from resource_params_tree import generate_resource_properties
+from common.utils import read_yaml
+
+
+def run(api_path, cloud_name, tags, output):
+
+    if api_path[-1] != "/":
+        api_path += "/"
+
+    product = read_yaml(api_path + "product.yaml")
+    if not product:
+        raise Exception("Read (%s) failed" % (api_path + "product.yaml"))
+
+    api_yaml = read_yaml(api_path + "api.yaml")
+    all_models = read_yaml(api_path + "models.yaml")
+
+    all_tags = {i["name"]: i for i in product["tags"]}
+    for tag in tags.split(","):
+        tag = tag.strip().decode("utf8")
+        if tag not in all_tags:
+            raise Exception("Unknown tag(%s)" % tag)
+
+        _, properties = generate_resource_properties(
+            api_yaml, all_models, tag,
+            read_yaml(api_path + tag + ".yaml")
+        )
+
+        write_file(output + tag + ".yaml", _generate_yaml(properties))
+
+
+def _generate_yaml(params, n=0):
+    r = []
+    keys = sorted(params.keys())
+    for k in keys:
+        v = params[k]
+        s = v.to_param_yaml(n)
+        if s:
+            r.extend(s)
+    return r
+
+
+def write_file(output, strs):
+    with open(output, "w") as o:
+        try:
+            o.writelines(strs)
+        except Exception:
+            try:
+                o.writelines(map(lambda s: s.encode("utf-8"), strs))
+            except Exception as ex:
+                raise Exception("Write schema result failed, %s" % ex)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 5:
+        print("Input docx dir, cloud name, "
+              "api tags(use , as delimiter), and output file")
+        sys.exit(1)
+
+    try:
+        run(*sys.argv[1:])
+
+    except Exception as ex:
+        print(ex)
+        sys.exit(1)
